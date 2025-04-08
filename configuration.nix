9571694b3c16e54bd82b072ad36c5a0bfe69d81f
typovrak <typovrak@gmail.com>
2025-04-09 00:48:29 +0200

    also a refacto

--- configuration.nix
+++ configuration.nix
@@ -10,21 +10,26 @@ in {
 		chown ${username}:${group} ${home}/.config
 		chmod 755 ${home}/.config
 
-		ln -sf ${./polybar} ${home}/.config/polybar
-		chown -h ${username}:${group} ${home}/.config/polybar
-		chmod -h 755 ${home}/.config/polybar
+		rm -rf ${home}/.config/polybar
+		mkdir ${home}/.config/polybar
+		chown ${username}:${group} ${home}/.config/polybar
+		chmod 755 ${home}/.config/polybar
 
-		chown -h ${username}:${group} ${home}/.config/polybar/config.ini
-		chmod -h 644 ${home}/.config/polybar/config.ini
+		cp ${home}/.config/polybar/config.ini
+		chown ${username}:${group} ${home}/.config/polybar/config.ini
+		chmod 644 ${home}/.config/polybar/config.ini
 
-		chown -h ${username}:${group} ${home}/.config/polybar/mocha.ini
-		chmod -h 644 ${home}/.config/polybar/mocha.ini
+		cp ${home}/.config/polybar/mocha.ini
+		chown ${username}:${group} ${home}/.config/polybar/mocha.ini
+		chmod 644 ${home}/.config/polybar/mocha.ini
 
-		chown -h ${username}:${group} ${home}/.config/polybar/audio.sh
-		chmod -h 755 ${home}/.config/polybar/audio.sh
+		cp ${home}/.config/polybar/audio.sh
+		chown ${username}:${group} ${home}/.config/polybar/audio.sh
+		chmod 755 ${home}/.config/polybar/audio.sh
 
-		chown -h ${username}:${group} ${home}/.config/polybar/brightnessctl.sh
-		chmod -h 755 ${home}/.config/polybar/brightnessctl.sh
+		cp ${home}/.config/polybar/brightnessctl.sh
+		chown ${username}:${group} ${home}/.config/polybar/brightnessctl.sh
+		chmod 755 ${home}/.config/polybar/brightnessctl.sh
 	'';
 
 	environment.systemPackages = with pkgs; [
