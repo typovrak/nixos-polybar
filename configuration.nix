0c41ac7e10282ccc013c5d2ae0d4724e75f0dee5
typovrak <typovrak@gmail.com>
2025-04-09 09:38:55 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -16,19 +16,19 @@ in {
 		chown ${username}:${group} ${home}/.config/polybar
 		chmod 700 ${home}/.config/polybar
 
-		cp ${./polybar/config.ini} ${home}/.config/polybar/config.ini
+		cp ${./config.ini} ${home}/.config/polybar/config.ini
 		chown ${username}:${group} ${home}/.config/polybar/config.ini
 		chmod 600 ${home}/.config/polybar/config.ini
 
-		cp ${./polybar/mocha.ini} ${home}/.config/polybar/mocha.ini
+		cp ${./mocha.ini} ${home}/.config/polybar/mocha.ini
 		chown ${username}:${group} ${home}/.config/polybar/mocha.ini
 		chmod 600 ${home}/.config/polybar/mocha.ini
 
-		cp ${./polybar/audio.sh} ${home}/.config/polybar/audio.sh
+		cp ${./audio.sh} ${home}/.config/polybar/audio.sh
 		chown ${username}:${group} ${home}/.config/polybar/audio.sh
 		chmod 700 ${home}/.config/polybar/audio.sh
 
-		cp ${./polybar/brightnessctl.sh} ${home}/.config/polybar/brightnessctl.sh
+		cp ${./brightnessctl.sh} ${home}/.config/polybar/brightnessctl.sh
 		chown ${username}:${group} ${home}/.config/polybar/brightnessctl.sh
 		chmod 700 ${home}/.config/polybar/brightnessctl.sh
 	'';
