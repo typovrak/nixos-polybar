8b421fbb4c08e8ae17880108cc59a512fb69eaaf
typovrak <typovrak@gmail.com>
2025-04-09 09:26:01 +0200

    test

--- configuration.nix
+++ configuration.nix
@@ -16,19 +16,19 @@ in {
 		chown ${username}:${group} ${home}/.config/polybar
 		chmod 700 ${home}/.config/polybar
 
-		cp ${home}/.config/polybar/config.ini
+		cp ${./polybar/config.ini} ${home}/.config/polybar/config.ini
 		chown ${username}:${group} ${home}/.config/polybar/config.ini
 		chmod 644 ${home}/.config/polybar/config.ini
 
-		cp ${home}/.config/polybar/mocha.ini
+		cp ${./polybar/mocha.ini} ${home}/.config/polybar/mocha.ini
 		chown ${username}:${group} ${home}/.config/polybar/mocha.ini
 		chmod 644 ${home}/.config/polybar/mocha.ini
 
-		cp ${home}/.config/polybar/audio.sh
+		cp ${./polybar/audio.sh} ${home}/.config/polybar/audio.sh
 		chown ${username}:${group} ${home}/.config/polybar/audio.sh
 		chmod 755 ${home}/.config/polybar/audio.sh
 
-		cp ${home}/.config/polybar/brightnessctl.sh
+		cp ${./polybar/brightnessctl.sh} ${home}/.config/polybar/brightnessctl.sh
 		chown ${username}:${group} ${home}/.config/polybar/brightnessctl.sh
 		chmod 755 ${home}/.config/polybar/brightnessctl.sh
 	'';
