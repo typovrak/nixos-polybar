b1db2edf25f1b8f0db91346e7b99353328bda6bc
typovrak <typovrak@gmail.com>
2025-04-27 22:27:22 +0200

    update

--- audio.sh
+++ audio.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
 
-echo "TESTTTTTTTTTT"
-exit 0
+vol_raw=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ 2>/dev/null)
 
-vol_raw=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-
-if [ -z "$vol_raw" ]; then
-    echo "%{F#a6e3a1}VOL%{F-} N/A"
-    exit 0
-fi
-
-percent=$(echo "$vol_raw" | grep -o '[0-9]*\.[0-9]*' | awk '{printf "%d", $1 * 100}')
-
-if [ -z "$vol_raw" ]; then
-    echo "%{F#a6e3a1}VOL%{F-} N/A"
+if [ -z "$vol_raw" ] || echo "$vol_raw" | grep -q "Translate id"; then
+    echo "%{F#a6e3a1}N/A%{F-}"
     exit 0
 fi
 
 mute=$(echo "$vol_raw" | grep -q MUTED && echo true || echo false)
+percent=$(echo "$vol_raw" | grep -o '[0-9]*\.[0-9]*' | awk '{printf "%d", $1 * 100}')
 
 if [ "$mute" = "true" ]; then
     echo "%{F#a6e3a1}MUTED%{F-}"
